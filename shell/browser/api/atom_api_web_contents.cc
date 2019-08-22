62ff37e6d026712c53d9e07480fa8eca9c4d58e2
Shelley Vohr <shelley.vohr@gmail.com>
2019-08-22 08:22:34 -0700

    update printing patch

--- shell/browser/api/atom_api_web_contents.cc
+++ shell/browser/api/atom_api_web_contents.cc
@@ -1672,6 +1672,8 @@ void WebContents::Print(mate::Arguments* args) {
 
     settings.SetString(printing::kSettingHeaderFooterTitle, header);
     settings.SetString(printing::kSettingHeaderFooterURL, footer);
+  } else {
+    settings.SetBoolean(printing::kSettingHeaderFooterEnabled, false);
   }
 
   // We don't want to allow the user to enable these settings
